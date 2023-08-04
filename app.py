81426c58717d847ab742b0d19673456ac99a3f77
renatosts <renatosts@gmail.com>
2023-08-04 01:13:25 -0300

    Ajuste design

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import streamlit as st
 import pandas as pd
 
 
-def define_color(val):
-    if val < 0:
-        color = 'red'
-    elif val > 0:
-        color = 'green'
-    else:
-        color = 'gray'
-    return 'color: %s' % color
-
-
 def make_clickable(link):
     # target _blank to open new window
     # extract clickable text to display for your link
@@ -32,6 +22,8 @@ st.set_page_config(
     page_title='SPFC')
 
 
+st.header('SÃO PAULO FUTEBOL CLUBE')
+
 f = 'https://raw.githubusercontent.com/renatosts/SPFC/main/SPFC.csv'
 #f = 'SPFC.csv'
 
@@ -41,21 +33,21 @@ df['data'] = pd.to_datetime(df['Data'], dayfirst=True)
 
 df = df.sort_values('data', ascending=False)
 
-df.Vídeo = df.Vídeo.fillna('').apply(make_clickable)
-
-df.index = df.index + 1
-
-df = df[['Data', 'Dia', 'Estádio', 'Tricolor', 'Pl1', 'Pl2', 'Adversário', 'Campeonato', 'Setor', 'Vídeo']]
+df['Dia'] = df['Dia'].str.capitalize()
 
 camp = df['Campeonato'].drop_duplicates().sort_values()
 
 advers = df['Adversário'].drop_duplicates().sort_values()
 
-col1, col2, col3 = st.columns(3)
+vde = ['Vitória', 'Empate', 'Derrota']
+
+col1, col2, col3, col4 = st.columns([2, 2, 1.5, 1])
 with col1:
     filt_advers = st.multiselect('Adversários', advers)
 with col2:
     filt_camp = st.multiselect('Campeonatos', camp)
+with col3:
+    filt_vde = st.multiselect('Resultados', vde)
 
 if filt_advers != []:
     df = df[df['Adversário'].isin(filt_advers)]
@@ -63,7 +55,23 @@ if filt_advers != []:
 if filt_camp != []:
     df = df[df['Campeonato'].isin(filt_camp)]
 
-df = df.to_html(escape=False)
+if filt_vde != []:
+    df = df[df['VDE'].isin([x[:1] for x in filt_vde])]
 
+df.Vídeo = df.Vídeo.fillna('').apply(make_clickable)
+
+df.index = df.index + 1
+
+df['Placar'] = df.Pl1.astype(str) + ' x ' + df.Pl2.astype(str)
+
+for col in ['Tricolor', 'Placar', 'Adversário']:
+    df[col] = df.apply(lambda row: 
+        f'<span style="font-weight:bold; color:darkgreen">{row[col]}</span>' if row.VDE == 'V' 
+        else f'<span style="font-weight:bold; color:darkred">{row[col]}</span>' if row.VDE == 'D' 
+        else f'<span style="font-weight:bold; color:goldenrod">{row[col]}</span>', axis=1)
+
+df = df[['Data', 'Dia', 'Estádio', 'Tricolor', 'Placar', 'Adversário', 'Campeonato', 'Setor', 'Vídeo']]
+
+df = df.to_html(escape=False)
 
 st.write(df, unsafe_allow_html=True)
